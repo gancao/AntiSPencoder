322faef0c3ab153b10173f1bafdb935097867d60
gancao <18883990694@163.com>
2026-01-26 18:38:47 +0800

    Add files via upload

--- script/AntiSP_analysis.py
+++ script/AntiSP_analysis.py
@@ -36,6 +36,28 @@ def get_cdr3_epitope():
 	seqs = list(set(seqs).difference(set(['nan'])))
 	return seqs
 
+def get_independent_test_list():
+	file = "../data/TCR_epitope/test_embbeding_data_info_filter_C_independent_seq_rm_sample0.05.txt"
+	info = pd.read_csv(file,sep="\t",header=0,index_col=0)
+	seq_ids = info['sequence'].dropna().tolist()
+	seqs = list(set(seq_ids))
+	seqs = list(set(seqs).difference(set(['nan'])))
+
+	# sampled_seqs = pandas_length_stratified_sample(seqs,sampling_ratio=0.1,seed=1996)
+
+	return seqs
+
+def get_independent_tcr_pmhc():
+	file = "../data/TCR_epitope/test_TCR_epitope_data_info_filter_C_human_independent_pmhc_rm.txt"
+	info = pd.read_csv(file,sep="\t",header=0,index_col=0)
+	epitope_ids = info['epitope'].dropna().tolist()
+	tra_seqs = info['cdr3_tra'].dropna().tolist()
+	trb_seqs = info['cdr3_trb'].dropna().tolist()
+	seqs = list(set(epitope_ids+tra_seqs+trb_seqs))
+	seqs = list(set(seqs).difference(set(['nan'])))
+	return seqs
+
+
 if __name__ == '__main__':
 	model_dir = "../model"
 	if torch.cuda.is_available():
@@ -56,26 +78,39 @@ if __name__ == '__main__':
 		param.requires_grad = False
 	pretrain.eval()
 
-	#1.huARdb
-	sequence = get_huARdb()
-	encoder_save_file = "../analysis/huARdb_v2_GEX.CD8/tcr_encoder_info.txt"
-	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=256,num_workers=4)
-	encoder_info.to_csv(encoder_save_file,sep="\t")
-	embed_save_file = "../analysis/huARdb_v2_GEX.CD8/tcr_embedding_info.txt"
-	embeddings_info.to_csv(embed_save_file,sep="\t")
-
-	#2.celltypist
-	sequence = get_celltypist()
-	encoder_save_file = "../analysis/celltypist/tcr_encoder_info.txt"
-	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=256,num_workers=4)
-	encoder_info.to_csv(encoder_save_file,sep="\t")
-	embed_save_file = "../analysis/celltypist/tcr_embedding_info.txt"
-	embeddings_info.to_csv(embed_save_file,sep="\t")
-
-	#3.for train，valid,test encoder
+	#for TCR-epitope
 	sequence = get_cdr3_epitope()
 	encoder_save_file = "../analysis/tcr_antigen_db/tcr_epitope_complete_cdr3_filter_encoder_info.txt"
 	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=32,num_workers=4)
-	encoder_info.to_csv(encoder_save_file,sep="\t")
 	embed_save_file = "../analysis/tcr_antigen_db/tcr_epitope_complete_cdr3_filter_embedding_info.txt"
 	embeddings_info.to_csv(embed_save_file,sep="\t")
+
+	#independent TCR dataset for AntiSPencoder
+	sequence = get_independent_test_list()
+	#for index,chunk in enumerate(sequence_chunks):
+	encoder_save_file = "../analysis/independent_test/All_AntiSPencoder_independent_test_encoder_info.txt"
+	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=32,num_workers=4)
+	encoder_info.to_csv(encoder_save_file,sep="\t")
+	embed_save_file = "../analysis/independent_test/All_AntiSPencoder_independent_test_embedding_info.txt"
+	embeddings_info.to_csv(embed_save_file,sep="\t")
+
+	#independent TCR-epitope dataset for AntiSPencoder
+	sequence = get_independent_tcr_pmhc()
+	encoder_save_file = "../analysis/independent_tcr_antigen_db/tcr_epitope_human_independent_filter_encoder_info.txt"
+	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=32,num_workers=4)
+	encoder_info.to_csv(encoder_save_file,sep="\t")
+	embed_save_file = "../analysis/independent_tcr_antigen_db/tcr_epitope_human_independent_filter_embedding_info.txt"
+	embeddings_info.to_csv(embed_save_file,sep="\t")
+
+	#celltypist
+	sequence = get_celltypist()
+	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=256,num_workers=4)
+	embed_save_file = "../analysis/celltypist/tcr_embedding_info.txt"
+	embeddings_info.to_csv(embed_save_file,sep="\t")
+
+	#huARdb
+	sequence = get_huARdb()
+	encoder_info,embeddings_info = pretrain.predict(sequence,device=device,batch_size=256,num_workers=4)
+	embed_save_file = "../analysis/huARdb_v2_GEX.CD8/tcr_embedding_info.txt"
+	embeddings_info.to_csv(embed_save_file,sep="\t")
+
